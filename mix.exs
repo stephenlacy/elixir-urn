06a6bdb5fc9e56b45fccd33679571279af485067
Matthew Johnston <warmwaffles@gmail.com>
2021-04-01 23:30:18 -0500

    Cleanup the mixfile

--- mix.exs
+++ mix.exs
@@ -1,42 +1,53 @@
-defmodule Urn.Mixfile do
+defmodule URN.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
-    [app: :urn,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     name: "Urn",
-     description: "Uniform Resource Name (URN) parsing and validation in Elixir",
-     source_url: "https://github.com/stevelacy/elixir-urn",
-     package: [licenses: ["MIT"],
-       maintainers: ["Steve Lacy"],
-       links: %{"Github" => "https://github.com/stevelacy/elixir-urn"}],
-     homepage_url: "https://github.com/stevelacy/urn",
-     docs: [main: "readme",
-       extras: ["README.md"]]]
+    [
+      app: :urn,
+      version: @version,
+      elixir: "~> 1.8",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      name: "URN",
+      description: description(),
+      source_url: "https://github.com/stevelacy/elixir-urn",
+      package: package(),
+      homepage_url: "https://github.com/stevelacy/urn",
+      docs: docs()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.15", only: :dev, runtime: false}]
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Uniform Resource Name (URN) parsing and validation in Elixir"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Steve Lacy", "Matthew Johnston"],
+      links: %{"Github" => "https://github.com/stevelacy/elixir-urn"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/stevelacy/elixir-urn"
+    ]
   end
 end
